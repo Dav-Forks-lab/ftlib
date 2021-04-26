b69f170b15f1556bba01b33576542b7a3bedd793
Zambo-dev <thomasz01@outlook.it>
2021-04-26 10:45:37 +0200

    Changed indentation from 4 to 8

--- src/ftlib.c
+++ src/ftlib.c
@@ -31,14 +31,14 @@ Struct
 * result_size    :  result array block lenght
 */
 typedef struct {
-    char *filename;
-    char *root_dir;
-    char *curr_dir;
-    char **win_disks;
-    char **result;
-    char *separator;
-    char **filters;
-    long int result_lenght, filter_lenght, result_size, win_disks_lenght;
+        char *filename;
+        char *root_dir;
+        char *curr_dir;
+        char **win_disks;
+        char **result;
+        char *separator;
+        char **filters;
+        long int result_lenght, filter_lenght, result_size, win_disks_lenght;
 } Folder;   
 
 /**
@@ -50,37 +50,37 @@ Init
 */
 void init(Folder *folder, const char* filename) 
 {      
-    /* Init sizes */
-    folder->result_size = 1024;
-    folder->result_lenght = 0;
-    folder->filter_lenght = 0;
+        /* Init sizes */
+        folder->result_size = 1024;
+        folder->result_lenght = 0;
+        folder->filter_lenght = 0;
 
-    folder->filters = malloc(FILTER_LIMIT * sizeof(char *));
-    folder->result = malloc(folder->result_size * sizeof(char *));
-    folder->filename = malloc(strlen(filename) + 1);
+        folder->filters = malloc(FILTER_LIMIT * sizeof(char *));
+        folder->result = malloc(folder->result_size * sizeof(char *));
+        folder->filename = malloc(strlen(filename) + 1);
 
-    /* Initialize directory based on the OS */
-    #ifdef _WIN32
-        folder->root_dir = malloc(strlen("C:\\") + 1);
-        strcpy(folder->root_dir, "C:\\");
-        folder->separator = malloc(strlen("\\") + 1);
-        strcpy(folder->separator, "\\");
-        /* Allocate disk array lenght */
-        folder->win_disks = malloc(sizeof(char*) * 32);
-        folder->win_disks_lenght = 0;
+        /* Initialize directory based on the OS */
+        #ifdef _WIN32
+                folder->root_dir = malloc(strlen("C:\\") + 1);
+                strcpy(folder->root_dir, "C:\\");
+                folder->separator = malloc(strlen("\\") + 1);
+                strcpy(folder->separator, "\\");
+                /* Allocate disk array lenght */
+                folder->win_disks = malloc(sizeof(char*) * 32);
+                folder->win_disks_lenght = 0;
 
-    #elif __linux__
-        folder->root_dir = malloc(strlen("/") + 1);
-        strcpy(folder->root_dir, "/");
-        folder->separator = malloc(strlen("/") + 1);
-        strcpy(folder->separator, "/");
+        #elif __linux__
+                folder->root_dir = malloc(strlen("/") + 1);
+                strcpy(folder->root_dir, "/");
+                folder->separator = malloc(strlen("/") + 1);
+                strcpy(folder->separator, "/");
 
-    #endif       
+        #endif       
 
-    folder->curr_dir = malloc(strlen(folder->root_dir) + 1);
+        folder->curr_dir = malloc(strlen(folder->root_dir) + 1);
 
-    strcpy(folder->filename, filename);    
-    strcpy(folder->curr_dir, folder->root_dir);
+        strcpy(folder->filename, filename);    
+        strcpy(folder->curr_dir, folder->root_dir);
 }
 
 /**
@@ -92,70 +92,70 @@ Find file
 */
 void find_file(Folder *folder)
 {   
-    char* directory = malloc(strlen(folder->curr_dir) + 1);
-    strcpy(directory, folder->curr_dir);
-    
-    DIR *dir;
-    struct dirent *ent;
+        char* directory = malloc(strlen(folder->curr_dir) + 1);
+        strcpy(directory, folder->curr_dir);
+        
+        DIR *dir;
+        struct dirent *ent;
 
-    if(strchr(directory, '.') != NULL)
-        return;
+        if(strchr(directory, '.') != NULL)
+                return;
 
-    /* Try to open the directory */
-    if((dir = opendir(directory)) != NULL)
-    {   
-        /* Search in every sub-folder */
-        while((ent = readdir(dir)) != NULL)
-        {  
-	        /* dev/fd folder not needed for the file searching */	
-            #ifdef __linux__
-	            if(strstr(ent->d_name, "fd") != NULL || strstr(ent->d_name, "proc") != NULL)
-		            continue;	
-	        #endif
-	        /* 
-            If the directory points to the same o the prev
-            directory the function retrun NULL
-            */
-            if(ent->d_name[strlen(ent->d_name) - 1] == '.' || ent->d_name[0] == '$')
-                continue;
+        /* Try to open the directory */
+        if((dir = opendir(directory)) != NULL)
+        {   
+                /* Search in every sub-folder */
+                while((ent = readdir(dir)) != NULL)
+                {  
+                        /* dev/fd folder not needed for the file searching */	
+                        #ifdef __linux__
+                        if(strstr(ent->d_name, "fd") != NULL || strstr(ent->d_name, "proc") != NULL)
+                                continue;	
+                        #endif
+                        /* 
+                        If the directory points to the same o the prev
+                        directory the function retrun NULL
+                        */
+                        if(ent->d_name[strlen(ent->d_name) - 1] == '.' || ent->d_name[0] == '$')
+                                continue;
 
-            if(folder->result_lenght == folder->result_size)
-            {   
-                int old_size = folder->result_size;
-                folder->result_size *= 2;
-                
-                char **template = malloc(folder->result_size * sizeof(char *));
-                memcpy(template, folder->result, old_size * sizeof(char *));
-                memset(folder->result, 0, old_size * sizeof(char *));
-                free(folder->result);
-                folder->result = template;
-            }
+                        if(folder->result_lenght == folder->result_size)
+                        {   
+                                int old_size = folder->result_size;
+                                folder->result_size *= 2;
+                                
+                                char **template = malloc(folder->result_size * sizeof(char *));
+                                memcpy(template, folder->result, old_size * sizeof(char *));
+                                memset(folder->result, 0, old_size * sizeof(char *));
+                                free(folder->result);
+                                folder->result = template;
+                        }
 
-            if(strstr(ent->d_name, folder->filename) != NULL)
-            {  
-                /* Set the necessary space for the dir */
-                folder->result[folder->result_lenght] = malloc(strlen(directory) + strlen(ent->d_name) +1);
-                /* Assemble the dir string */
-                strcpy(folder->result[folder->result_lenght], directory);
-                strcat(folder->result[folder->result_lenght], ent->d_name);
-                /* Increase the dir index */
-                folder->result_lenght++;
-            }
+                        if(strstr(ent->d_name, folder->filename) != NULL)
+                        {  
+                                /* Set the necessary space for the dir */
+                                folder->result[folder->result_lenght] = malloc(strlen(directory) + strlen(ent->d_name) +1);
+                                /* Assemble the dir string */
+                                strcpy(folder->result[folder->result_lenght], directory);
+                                strcat(folder->result[folder->result_lenght], ent->d_name);
+                                /* Increase the dir index */
+                                folder->result_lenght++;
+                        }
 
-            /* Set current directory */
-            folder->curr_dir = realloc(folder->curr_dir, strlen(directory) + strlen(ent->d_name) + strlen(folder->separator) + 2);
-            strcpy(folder->curr_dir, directory);
-            strcat(folder->curr_dir, ent->d_name);
-            strcat(folder->curr_dir, folder->separator);
-            /* Start the search again with recursion */
-            find_file(folder);
+                        /* Set current directory */
+                        folder->curr_dir = realloc(folder->curr_dir, strlen(directory) + strlen(ent->d_name) + strlen(folder->separator) + 2);
+                        strcpy(folder->curr_dir, directory);
+                        strcat(folder->curr_dir, ent->d_name);
+                        strcat(folder->curr_dir, folder->separator);
+                        /* Start the search again with recursion */
+                        find_file(folder);
+                }
         }
-    }
-    
-    /* Clear pointer data */
-    closedir(dir);
-    free(dir);
-    free(ent);
+        
+        /* Clear pointer data */
+        closedir(dir);
+        free(dir);
+        free(ent);
 }
 
 
@@ -168,15 +168,15 @@ Set filter
 */
 void set_filter(Folder *folder, char* new_filter[], int filter_len)
 {   
-    for(int i = 0; i < filter_len; i++)
-    {   
-        if(i < FILTER_LIMIT)
+        for(int i = 0; i < filter_len; i++)
         {   
-            folder->filters[i] = malloc(strlen(new_filter[i]) + 1);
-            strcpy(folder->filters[i], new_filter[i]);
-            folder->filter_lenght++;
+                if(i < FILTER_LIMIT)
+                {   
+                        folder->filters[i] = malloc(strlen(new_filter[i]) + 1);
+                        strcpy(folder->filters[i], new_filter[i]);
+                        folder->filter_lenght++;
+                }
         }
-    }
 }
 
 /**
@@ -188,21 +188,21 @@ Apply filter
 */
 void apply_filter(Folder *folder, char* filtered_result[], int* index)
 {   
-    for(int i=0; i < folder->result_lenght; i++)
-    {   
-        for(int j=0; j < folder->filter_lenght; j++)
-        {
-            /* Check if the result contains the filter */
-            if(strstr(folder->result[i], folder->filters[j]) != NULL)
-            {   
-                filtered_result[*index] = malloc(strlen(folder->result[i]) +1);
-                strcpy(filtered_result[*index], folder->result[i]);
-                
-                (*index)++;
-                break;
-            }
+        for(int i=0; i < folder->result_lenght; i++)
+        {   
+                for(int j=0; j < folder->filter_lenght; j++)
+                {
+                        /* Check if the result contains the filter */
+                        if(strstr(folder->result[i], folder->filters[j]) != NULL)
+                        {   
+                                filtered_result[*index] = malloc(strlen(folder->result[i]) +1);
+                                strcpy(filtered_result[*index], folder->result[i]);
+                                
+                                (*index)++;
+                                break;
+                        }
+                }
         }
-    }
 }
 
 /**
@@ -214,8 +214,8 @@ Change filename
 */
 void change_filename(Folder* folder, char* new_filename)
 {
-    folder->filename = realloc(folder->filename, strlen(new_filename) + 1);
-    strcpy(folder->filename, new_filename);
+        folder->filename = realloc(folder->filename, strlen(new_filename) + 1);
+        strcpy(folder->filename, new_filename);
 }
 
 /**
@@ -227,8 +227,8 @@ Change root dir
 */
 void change_root_directory(Folder* folder, char* new_root_folder)
 {
-    folder->root_dir = realloc(folder->root_dir, strlen(new_root_folder) +1);
-    strcpy(folder->root_dir, new_root_folder);
+        folder->root_dir = realloc(folder->root_dir, strlen(new_root_folder) +1);
+        strcpy(folder->root_dir, new_root_folder);
 }
 
 /**
@@ -240,16 +240,16 @@ Print result
 */
 int print(Folder* folder)
 {   
-    /* Check if the array is non empty */
-    if(folder->result[0])
-        /* Print array data */
-        for(int i=0; i < folder->result_lenght; i++)
-            printf("%s\n", folder->result[i]);
-    else 
-        /* If the array is empty return an error code */
-        return 1;
+        /* Check if the array is non empty */
+        if(folder->result[0])
+                /* Print array data */
+                for(int i=0; i < folder->result_lenght; i++)
+                printf("%s\n", folder->result[i]);
+        else 
+                /* If the array is empty return an error code */
+                return 1;
 
-    return 0;
+        return 0;
 }
 
 /**
@@ -260,6 +260,6 @@ Windows disk
 * Include a file for windows disk listing
 */
 #ifdef _WIN32
-    #include "win_functions.h"
+        #include "win_functions.h"
 #endif 
 
