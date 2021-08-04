904bebb71ef7907dc4ef70a8ab3c86870e48450a
Zambo-dev <thomasz01@outlook.it>
2021-08-04 19:54:11 +0200

    linux test update

--- test/linux_test.c
+++ test/linux_test.c
@@ -21,21 +21,18 @@ void print_data(Folder* folder)
 {
         puts("Thread 2 started");
         long int old = 0;
-        long int idx = 0;
-        char* array[4096];
-        long size[4096];
+
         while(folder->result_index != folder->result_length || print_data_on)
         {       
                 if(old != 0 && strcmp(folder->curr_dir, "/") == 0)
                         continue;
                 if(old != folder->result_length)
                 {       
-                        idx = apply_filter(folder, array, size, idx);
-                        for(int i=old; i < idx; i++)
+                        for(int i=old; i < folder->result_length; i++)
                         {
-                            printf("\n--> %s %ld", array[i], size[i]);
+                            printf("\n--> %s %ld", folder->result_array[i], folder->file_size_array[i]);
                         }
-                        old = idx;
+                        old = folder->result_length;
                 }
         }
 }
@@ -45,21 +42,16 @@ int main()
         Folder *f = malloc(sizeof(Folder));
         pthread_t thread1, thread2;
         char* fName = malloc(1024);
-        char* filter = malloc(1024);
 
         printf("\nInserisci il nome del file: ");
         scanf("%s", fName);
 
         init(f, fName);
 
-        printf("\nInserisci il filtro: ");
-        scanf("%s", filter);
-
-        add_filter(f, filter);
-
         pthread_create(&thread2, NULL, (void*)print_data, (void*)f);
     
         pthread_create(&thread1, NULL, (void*)find_file, (void*)f);    
+
         pthread_join(thread1, NULL);
 
         print_data_on = 0;
